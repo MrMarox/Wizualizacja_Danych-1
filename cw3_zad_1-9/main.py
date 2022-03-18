93143ca9786c131923f19d1d3c10cd38946fb0ac
Bartosz Radzanowski <92019697+rodzyn997@users.noreply.github.com>
2022-03-18 14:54:12 +0100

    zad3_1-9

---
+++ cw3_zad_1-9/main.py
@@ -0,0 +1,84 @@
+import sys
+import math
+import random
+
+# print('zad 1')
+# listaA = []
+# for x in range(1,11):
+#     listaA.append(1-x)
+# print(listaA)
+#
+# listaB = []
+# for x in range(0,8):
+#     listaB.append(4**x)
+# print(listaB)
+#
+# listaC= []
+# for x in listaC:
+#     if x % 2 == 0:
+#         listaA.append(x)
+# print(listaC)
+#
+# print('zad 2')
+#
+# listaD = []
+# for x in range(0,11):
+#     listaD.append(random.randint(0, 100))
+# print(listaD)
+# listaE = []
+# for x in d:
+#     if x % 2 == 0:
+#         listaE.append(x)
+# print(listaE)
+#
+# print('zad 3')
+#
+# slownik = {'Ciastka': 'kg', 'Banany': 'kg',  'Bułki': 'szt', 'Batony': 'szt'}
+#
+# lista = []
+# print(slownik)
+# for key, value in slownik.items():
+#     if slownik[key] == 'szt':
+#         lista.append(key)
+#
+# print(lista)
+
+# print('zad 4')
+#
+# def czyprostokat(a,b,c):
+#     if pow(a,2)+pow(b,2) == pow(c,2):
+#         print('jest prostokatny')
+#     else:
+#         print('nie jest prostokatny')
+# print(czyprostokat(3,4,5))
+#
+# print('zad 5')
+#
+# def poltrap(a=2,b=3,h=2):
+#     pole =((a+b)*h)/2
+#     return pole
+# print(poltrap())
+#
+# print('zad 6')
+#
+# def ciag(a1=1,b=2,ile=10):
+#     for x in range(ile):
+#         a1 = a1*b
+#     return a1
+#
+# print(ciag())
+#
+#
+# print('zad 7')
+# def ciag(*licz):
+#     if len(licz) == 0:
+#         return 0
+#     else:
+#         ilo=1
+#         for x in licz:
+#             ilo=ilo*x
+#         return ilo
+#     print(ciag(1,2,3,4,5))
+
+
+
