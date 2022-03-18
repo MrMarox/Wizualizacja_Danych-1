a0c49456b90bc03317d1d321ae86be7d0c2d3a17
Bartosz Radzanowski <92019697+rodzyn997@users.noreply.github.com>
2022-03-18 14:35:39 +0100

    zad2_1-9

---
+++ cw2_zad_1-9/main.py
@@ -0,0 +1,77 @@
+# print('zad.1')
+#
+# lista_ulub = ['piłka nożna', 'koszykówka', 'siatkówka']
+# print(lista_ulub)
+# lista_ulub.reverse()
+# print(lista_ulub)
+# lista_ulub.append('hokej')
+# print('mniej lubiany sport na koncu: ',lista_ulub)
+#
+# print('zad.2')
+#
+# slownik_skrot ={'itd.':'i tak dalej', 'itp.':'i tym podobniej'}
+# for x in slownik_skrot:
+#     print(slownik_skrot[x])
+#
+# print('zad.3')
+# slownik_ulub={'COD':'Call of Duty','GTA V':'Grand theft auto V'}
+# print(len(slownik_ulub))
+#
+# print('zad.4')
+# liter_a = 0
+# n4= input("wprowadz napis: ")
+# for x in n4:
+#     if x =='a':
+#      liter_a+=1
+# print('tyle jest liter a: ', liter_a)
+#
+# print('zad.5')
+#
+# import sys as system
+#
+# system.stdout.write('Podaj wartosc a: ')
+# a5= system.stdin.readline()
+# system.stdout.write('Podaj wartosc b: ')
+# b5= system.stdin.readline()
+# system.stdout.write('Podaj wartosc c: ')
+# c5= system.stdin.readline()
+#
+# a5 = int(a5)
+# b5 = int(b5)
+# c5 = int(c5)
+# wyn5=pow(a5,b5)+c5
+# wyn5 = str(wyn5)
+#
+# system.stdout.write(wyn5)
+
+# print('zad.6')
+# a = input('podaj a: ')
+# b = input('podaj b: ')
+# c = input('podaj c: ')
+#
+# if b>a and b>c:
+#     print('b jest najwieksza')
+# if a>b and a>c:
+#     print('a jest najwieksze')
+# if c>b and c>a:
+#     print('c jest najwieksza')
+#
+#
+# print('zad.7')
+# lista7= [6,2,3.3,4.5]
+# for x in lista7:
+#     x=pow(x,2)
+#     print(x)
+# print('zad.8')
+#
+# lista8=[]
+# licznik=0
+# while licznik<11:
+#     x=input("podaj liczby")
+#     if(x%2==0):
+#         lista8.append(x)
+# licznik+=1
+# print(lista8)
+#
+# print('zad.9')
+#  nie wiem
