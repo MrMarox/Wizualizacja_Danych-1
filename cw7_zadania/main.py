af823d15c1585011a2f670006b8096da3e23d885
Bartosz Radzanowski <92019697+rodzyn997@users.noreply.github.com>
2022-04-20 13:17:39 +0200

    20.04

---
+++ cw7_zadania/main.py
@@ -0,0 +1,73 @@
+import numpy as np
+import math
+# zad 1
+
+# a=np.array([1,3,2])
+# b=np.array([6,4,2])
+# c=a*b
+# print(c)
+
+# zad 2
+# nie ma
+
+# zad 3
+
+# a=np.array([1,3,2])
+# b=np.array([6,4,2])
+# c=a.dot(b)
+# print(c)
+
+# zad 4
+
+# a=np.array([1,3,2])
+# b=np.array([-2,-4,2])
+# c=a*b
+# print(c)
+
+# zad 5
+
+# a=np.array([[1,3,4],[1,2,2]])
+#
+# a=np.sin(a)
+#
+# print(a)
+
+# zad 6
+
+# a=np.array([[1,3,4],[1,2,2]])
+# a=np.cos(a)
+# print(a)
+
+# zad 7
+
+# a= np.array([[1,3],[3,4]])
+# b= np.array([[8,7],[1,5]])
+# a+=b
+# print(a)
+
+# zad 8
+# a= np.random.randint(0,100,(3,3))
+# for x in a.flat:
+#     print(x)
+
+# zad 9
+
+# a= np.random.randint(0,100,(3,3))
+# for x in a.flat:
+#     print(x)
+
+# zad 10
+
+# a= np.random.randint(0,100,(9,9))
+# print(a)
+# b=a.reshape((1,81))
+# c=a.reshape((81,1))
+# d=a.reshape((27,3))
+# e=a.reshape((3,27))
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+
+# zad 11
+# nie ma
